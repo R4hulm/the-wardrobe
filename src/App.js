432d3d6e2824e0900175f863ec8de4563005e4a0
unknown <rahulmurugesh.work@gmail.com>
2022-03-15 23:18:12 +0530

    added logo and navigation functionality to navigation comoponent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import {Routes,Route, Outlet} from 'react-router-dom';
+import {Routes,Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
+
 
 const Shop =() =>{
   return (
@@ -7,17 +10,6 @@ const Shop =() =>{
   );
 }
 
-const Navigation = () =>{
-
-  return (
-    <div>
-      <div>
-        <h1> navigation</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-}
 
 const App = () => {
   return (
@@ -25,6 +17,7 @@ const App = () => {
       <Route path='/' element= {<Navigation />} >
         <Route index element= {<Home />} />
         <Route path='shop' element= {<Shop />} />
+        <Route path='sign-in' element= {<SignIn />} />
       </Route>
     </Routes>
   
