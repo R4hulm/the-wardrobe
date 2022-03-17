67271451b7c82b2b039b72a86a1b52203f46b008
unknown <rahulmurugesh.work@gmail.com>
2022-03-17 23:56:51 +0530

    updated user context

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,4 +1,5 @@
-import { createContext ,useState} from 'react';
+import { createContext ,useState, useEffect} from 'react';
+import { onAuthStateChangedListener } from '../utils/firebase/firebase.utils';
 
 export const UserContext = createContext({
     currentUser:null,
@@ -9,6 +10,13 @@ export const UserContext = createContext({
 export const UserProvider = ({ children }) => {
     const [currentUser,setCurrentUser] = useState(null);
     const value = {currentUser, setCurrentUser};
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedListener((user) => {
+            setCurrentUser(user);
+        });
+        return unsubscribe
+    },[]);
 
     return <UserContext.Provider value={value}> { children }</UserContext.Provider>
 }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,7 +5,8 @@ import {
     GoogleAuthProvider, 
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
-    signOut
+    signOut,
+    onAuthStateChanged
 } from 'firebase/auth';
 import {
     getFirestore,
@@ -77,3 +78,6 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
   return await signInWithEmailAndPassword(auth, email, password);
 };
 export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener  = (callback) => 
+onAuthStateChanged(auth,callback);
