3bc3bd37ff5a7d4610e9f199ec8551377b2bd5e0
unknown <rahulmurugesh.work@gmail.com>
2022-05-07 22:16:51 +0530

    displaying category items using redux-saga

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,6 +1,6 @@
 import { createAction } from "../../utils/reducer/reducer.utils";
 import { CATEGORIES_ACTION_TYPES } from "./category.types";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+
 
 export const setCategories = (categoriesMap) => 
     createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesMap);
@@ -13,14 +13,3 @@ export const fetchCategoriesSuccess = (categoriesMap) =>
 
 export const fetchCategoriesFailed = () => 
     createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED);
-
-export const fetchCategoriesAsync = () => async (dispatch) => {
-    dispatch(fetchCategoriesStart());
-
-    try{
-        const categoriesArray = await getCategoriesAndDocuments('categories');
-        dispatch(fetchCategoriesSuccess(categoriesArray));
-    }catch(error) {
-        dispatch(fetchCategoriesFailed(error))
-    }
-}
--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,8 @@ import {compose, createStore, applyMiddleware } from 'redux';
 import {persistStore, persistReducer} from 'redux-persist';
 import logger from 'redux-logger';
 import storage from 'redux-persist/lib/storage';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import {rootSaga} from './root-saga';
 
 import { rootReducer } from './root-reducer';
 
@@ -13,10 +14,12 @@ const persistConfig = {
   whitelist: ['cart'],
 }
 
+const sagaMiddleware = createSagaMiddleware();
+
 const persistedReducer = persistReducer(persistConfig,rootReducer);
 
 const middleWares = [process.env.NODE_ENV !== 'production' && logger, 
-thunk,
+sagaMiddleware,
 ].filter(Boolean);
 
 const composeEnhancer = (
@@ -33,5 +36,7 @@ export const store = createStore(
   undefined, 
   composedEnhancers
   );
+
+  sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
