1e178ac34f4ec468ac61bfdeb876f31d5fbf5c68
unknown <rahulmurugesh.work@gmail.com>
2022-05-07 13:05:34 +0530

    added redux-persist and updated store component

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { render } from "react-dom";
 import {BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
-import { store } from './store/store';
+import { store ,persistor} from './store/store';
 import {Elements} from "@stripe/react-stripe-js";
 import { stripePromise } from './utils/stripe/stripe.utils';
 import './index.scss';
@@ -13,11 +14,13 @@ const rootElement = document.getElementById("root");
 render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Elements stripe={stripePromise}>
-          <App />
-        </Elements>
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   rootElement
--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,29 @@
 import {compose, createStore, applyMiddleware } from 'redux';
+import {persistStore, persistReducer} from 'redux-persist';
 import logger from 'redux-logger';
+import storage from 'redux-persist/lib/storage';
 
 import { rootReducer } from './root-reducer';
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
-    Boolean
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig,rootReducer);
+
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(
+  Boolean
   );
-// const loggerMiddleWare = (store) => (next) => (action) => {
-//   if(!action.type) {
-//     return next(action);
-//   }
-// }
+
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer, 
+  undefined, 
+  composedEnhancers
+  );
+
+export const persistor = persistStore(store);
